c62cb1ebc1b19aff20fb6590a2e9c2c877ea3ff8
ayush sharma <ayush.aceit@gmail.com>
2016-03-02 03:04:35 +0530

    scaffolfing

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yeomanD3App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yeomanD3App
+ */
+angular.module('yeomanD3App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
